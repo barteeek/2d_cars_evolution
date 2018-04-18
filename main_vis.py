d29f676252531355ed53faf220ae6f9636fc9be2
Igorek <igor.gajowiak@gmail.com>
2018-04-18 23:04:54 +0200

    visualize dumped objects

--- main_vis.py
+++ main_vis.py
@@ -1,5 +1,17 @@
+import argparse
+import pickle
 from visualization.playground import Playground
 from visualization.framework import main
 
 if __name__ == "__main__":
-    main(Playground)
+    parser = argparse.ArgumentParser(description='Evo visualization run')
+    parser.add_argument('input_dir', type=str,
+                        help='input directory to get data from')
+    parser.add_argument('iteration_it', type=str,
+                        help='iteration from which we want to get data')
+    args = parser.parse_args()
+
+    with open(args.input_dir + '/' + 'iteration_' + args.iteration_it, 'rb') as handle:
+        dict = pickle.load(handle)
+
+    main(Playground, dict["best"])
--- visualization/playground.py
+++ visualization/playground.py
@@ -11,7 +11,7 @@ class Playground (Framework):
     bridgePlanks = 20
     counter = 0
 
-    def __init__(self):
+    def __init__(self, car):
         super(Playground, self).__init__()
 
         terrain = Terrain(100, 2.)
@@ -28,7 +28,7 @@ class Playground (Framework):
             (-1.15, 0.9),
             (-1.5, 0.2),]),
         [WheelRepresentation([-1., -1], 0., 0.4), WheelRepresentation([1, -1], 0., 0.4)])
-        self.c = CarRepresentation()
+        self.c = car
         car, wheels, springs = self.c.put_to_world(self.world)
 
         self.car = car
